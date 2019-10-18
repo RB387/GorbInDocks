2698a61796345aab41ec23dc3d8a21b112dadac3
RB387 <53271136+RB387@users.noreply.github.com>
2019-10-18 22:21:09 +0300

    Delete password_validate.js
    was moved to /static

--- templates/script/password_validate.js
+++
@@ -1,9 +0,0 @@
-function validatePassword(){
-	var password = document.getElementById("pass")
-	, confirm_password = document.getElementById("pass_conf");
-	if(password.value != confirm_password.value) {
-		confirm_password.setCustomValidity("Passwords Don't Match");
-	} else {
-		confirm_password.setCustomValidity('');
-	}
-}
