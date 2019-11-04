a0636658dc835af703d2a050bc22b1f19fcc5aba
Faralaks <Faralaks@ya.ru>
2019-11-04 20:30:31 +0300

    added add_folder function to create folders. add_file and add_folder now take the _id of the folder it is in ('/' for the root folder)

--- mongo_test.py
+++ mongo_test.py
@@ -16,7 +16,6 @@ class flask_g():
         self.files = self.db['test_files']
 
 
-
 class mongo_test(unittest.TestCase):
     def test_add_user(self):
         log = 'log1'
@@ -29,16 +28,63 @@ class mongo_test(unittest.TestCase):
         self.assertEqual(get['login'], log)
         self.assertEqual(get['pas'], hash(pas))
         self.assertEqual(get['email'], mail)
+        self.assertEqual(get['shared'], [])
 
 
-    def test_remake_user(self):
-        remake_users(g, 'yes')
-        size = g.users.count_documents({})
 
-        self.assertEqual(size, 0)
+    def test_add_folder(self):
+        owner = 'log2'
+        name = 'name2'
+        location = 'C:\\tesk_loc'
+        size = 10
+
+        new = add_folder(g, owner, name, size, location)
+        get = g.files.find_one({'_id':new})
+        NEW = new
+        
+        self.assertEqual(get['owner'], owner)
+        self.assertEqual(get['name'], name)
+        self.assertEqual(get['size'], size)
+        self.assertEqual(get['location'], location)
+        self.assertEqual(get['dir'], '/')
+        self.assertEqual(get['shared'], [])
+
+        owner = 'log1'
+        name = 'name1'
+        location = 'C:\\tesk_loc'
+        size = 10
+        new = add_file(g, owner, name, size, location, NEW)
+        get = g.files.find_one({'_id':new})
+        
+        self.assertEqual(get['owner'], owner)
+        self.assertEqual(get['name'], name)
+        self.assertEqual(get['size'], size)
+        self.assertEqual(get['location'], location)
+        self.assertEqual(get['dir'], NEW)
+        self.assertEqual(get['shared'], [])
+
+    
+
+
+if 1:
+    class remake_test(unittest.TestCase):
+        def test_remake_users(self):
+            remake_users(g, 'yes')
+            size = g.users.count_documents({})
+
+            self.assertEqual(size, 0)
+
+        def test_remake_files(self):
+            remake_files(g, 'yes')
+            size = g.files.count_documents({})
+
+            self.assertEqual(size, 0)
+
 
 
 if __name__ == '__main__':
     g = flask_g()
+    remake_users(g, 'yes')
+    remake_files(g, 'yes')
     unittest.main()
 
