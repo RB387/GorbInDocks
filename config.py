6b0900f438f2d9a8b018081f3b4233f27fabdba3
RB387 <53271136+RB387@users.noreply.github.com>
2019-11-07 11:07:37 +0300

    Update config.py
    Added ZIP_FOLDER field

--- config.py
+++ config.py
@@ -1,6 +1,7 @@
 from os import path, getcwd
 SECRET_KEY = '1337' # key to secret sessions. keep it a complete secret!
 UPLOAD_FOLDER = path.join(getcwd(), 'data') # ./data as default. you can enter your path
+ZIP_FOLDER = path.join(getcwd(), 'temp_data') #./temp_data as default
 DB_NAME = 'gorbin' # name of Mongo database
 USERS_COL_NAME = 'users' # name of the users collection 
 FILES_COL_NAME = 'files' # name of the files collection
