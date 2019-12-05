507ff6188fd2f9490d4fe5df67b7bb34557371d3
RB387 <53271136+RB387@users.noreply.github.com>
2019-12-05 10:53:36 +0300

    Update main.py

--- main.py
+++ main.py
@@ -392,6 +392,7 @@ if __name__ == '__main__':
 
 	if setup:
 		with app.app_context():
+			gt.add_user(self, login = 'admin', pas = gorbin_tools2.hash('admin'), email = 'xd@yolo.com', status='admin')
 			gt.remake_files('yes')
 			gt.remake_users('yes')
 			gt.remake_links('yes')
