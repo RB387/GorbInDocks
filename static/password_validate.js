ab9c4eded0fd0b192d7a5e484cc537e1fc210e9e
RB387 <zavadin142@yandex.ru>
2019-12-16 00:22:29 +0300

    Removed old files

--- static/password_validate.js
+++
@@ -1,9 +0,0 @@
-function validatePassword(){
-		var password = document.getElementById("pass")
-		, confirm_password = document.getElementById("pass_conf");
-		if(password.value != confirm_password.value) {
-				confirm_password.setCustomValidity("Passwords doesn't match");
-		} else {
-				confirm_password.setCustomValidity('');
-		}
-}
