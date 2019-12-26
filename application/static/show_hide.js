96191e6663479972d968df2578885caed5d35a46
RB387 <zavadin142@yandex.ru>
2019-12-26 22:05:10 +0300

    Minor changes

--- application/static/show_hide.js
+++ application/static/show_hide.js
@@ -8,4 +8,11 @@ function show_hide(elements) {
         }
     }
 }
+function openNav() {
+    document.getElementById("mySidenav").style.width = "250px";
+}
+  
+function closeNav() {
+    document.getElementById("mySidenav").style.width = "0";
+}
                         
